bac6923fa7901dc87177d1fa10a1d74b450f7bb1
flapjackstan <elm.s.camargo@gmail.com>
2022-02-16 07:26:29 -0800

    added elimination list version

--- two_sum.py
+++ two_sum.py
@@ -40,12 +40,20 @@ class Solution:
                     return [index1, index2]
                     
 
+    def two_sum_elim_list(self, nums: list[int], target: int) -> list[int]:
+        print(f'list:{nums}', f'target: {target}')
+        
+        for index1, item1 in enumerate(nums):
+            
+            for index2, item2 in enumerate(nums[ index1 + 1 : ]):
 
+                if item1 + item2 == target:
+                    return [index1, index1 + index2 + 1]
 
 def main():
     solution = Solution()
 
-    two_sum = solution.two_sum_list([1,12,45,60,75,76,78,90], 102)
+    two_sum = solution.two_sum_elim_list([1,12,45,60,75,76,78,90], 102)
     print('solution:' + str(two_sum))
 
 if __name__ == '__main__':
